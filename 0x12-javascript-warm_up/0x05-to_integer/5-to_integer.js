2fa09e010ce1b41301ea6552f85f0299874aeda6
Ayodele Kadiri <kadelcode@gmail.com>
2024-02-09 22:28:46 +0100

    convert first argument to an integer

---
+++ 0x12-javascript-warm_up/0x05-to_integer/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+const firstArg = parseInt(argv[2]);
+
+if (isNaN(firstArg)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${firstArg}`);
+}
