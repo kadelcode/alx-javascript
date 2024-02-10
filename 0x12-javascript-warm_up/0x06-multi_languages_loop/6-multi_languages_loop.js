407b9a05103a023740af09b36a00c16962e76137
Ayodele Kadiri <kadelcode@gmail.com>
2024-02-10 21:17:38 +0100

    Loop to languages

---
+++ 0x12-javascript-warm_up/0x06-multi_languages_loop/6-multi_languages_loop.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * A script that prints 3 line by using an array of string and a loop
+ * first line: "C is fun"
+ * second line: "Python is cool"
+ * third line: "JavaScript is amazing"
+ *
+ */
+
+const arrayString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const i of arrayString) {
+  console.log(i);
+}
