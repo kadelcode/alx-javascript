0fb67325e44018e65f90e2b93d8b823e05b3471f
Ayodele Kadiri <kadelcode@gmail.com>
2024-02-18 20:08:28 +0100

    Call me Moby

---
+++ 0x12-javascript-warm_up/0xF-call_me_moby/101-call_me_moby.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/*
+ * A file containing a function that
+ * executes `x` times a function
+ *
+ */
+
+exports.callMeMoby = function (x, theFunction) {
+	for (let i = 0; i < x; i++) {
+		theFunction()
+	}
+}
