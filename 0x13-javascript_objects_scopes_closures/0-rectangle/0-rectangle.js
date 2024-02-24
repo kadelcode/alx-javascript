460b60a0ed18a81d5b966ea6c124286c8614bd8f
Ayodele Kadiri <kadelcode@gmail.com>
2024-02-24 20:20:07 +0100

    Rectangle #0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle/0-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// A javaScript file containing an empty class
+
+class Rectangle {
+  // an empty class
+}
+
+// export class so that it can be used in other files
+module.exports = Rectangle;
