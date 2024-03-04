a01eb3d4b84214086dd6d37e65ef4f630bf6a2ea
Ayodele Kadiri <kadelcode@gmail.com>
2024-03-04 18:57:19 +0100

    Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever/8-esrever.js
@@ -0,0 +1,15 @@
+// 8-esrever.js
+
+exports.esrever = function (list) {
+  // a function that returns the reversed version of a list
+
+  // create an empty list
+  const revList = [];
+
+  // let listSize = list.length - 1;
+  for (let i = list.length - 1; i => 0; i--) {
+    revList.push(list[i]);
+  }
+
+  return revList;
+};
