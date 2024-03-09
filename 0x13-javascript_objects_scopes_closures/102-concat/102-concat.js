99c73bf50c97fe93236e6f55a34cc7085ff17083
Ayodele Kadiri <kadelcode@gmail.com>
2024-03-09 21:34:18 +0100

    Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat/102-concat.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// 102-concat.js
+
+
+const fs = require('fs') // import file system
+// const { argv } = require('process') // import node process
+
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+const content1 = fs.readFileSync(sourceFile1, 'utf-8');
+const content2 = fs.readFileSync(sourceFile2, 'utf-8');
+
+const concatenatedContent = content1 + content2;
+
+fs.writeFileSync(destinationFile, concatenatedContent)
