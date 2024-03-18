7e37c4740a8b5a8696a3effa7529ba2f082932f2
Ayodele Kadiri <kadelcode@gmail.com>
2024-03-18 20:20:52 +0100

    add request package

---
+++ 0x14-javascript-web_scraping/2-statuscode/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, res) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`code: ${res.statusCode}`);
+});
