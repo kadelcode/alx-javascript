6eda94e0be5b02a9c041ca5fc158aeafd4769408
Ayodele Kadiri <kadelcode@gmail.com>
2024-03-20 20:11:00 +0100

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title/3-starwars_title.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+});
