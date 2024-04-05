e67f351fdc3608f2c4736a171585717de3cbc4e9
Ayodele Kadiri <kadelcode@gmail.com>
2024-04-05 18:33:57 +0100

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks/6-completeed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTasksByUser[todo.userId]) {
+        completedTasksByUser[todo.userId]++;
+      } else {
+        completedTasksByUser[todo.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
