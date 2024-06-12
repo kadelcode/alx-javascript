c69761478c0e0c9d6a7e6cd8717f82ef21f97d86
Ayodele Kadiri <kadelcode@gmail.com>
2024-06-12 20:32:32 +0100

    Say hrly hello to everybody!

---
+++ 0x15-javascript-web_jquery/12-script/102-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('#btn_translate').click(function() {
+    const langCode = $('#language_code').val();
+    const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`;
+    
+    $.ajax({
+      url: apiUrl,
+      dataType: "json",
+      success: function(data) {
+        $('#hello').txt(data.hello);
+      }
+    });
+  });
+});
